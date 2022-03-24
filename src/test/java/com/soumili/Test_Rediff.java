1eaa11fa258a15461174390656dec9185d40d1e3
Soumili_Roy <Soumili.Roy@skillassure.com>
2022-03-24 16:54:03 +0530

    Change

--- src/test/java/com/soumili/Test_Rediff.java
+++ src/test/java/com/soumili/Test_Rediff.java
@@ -8,6 +8,8 @@ public class Test_Rediff {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/resources/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Hello World");
 
 	}
 
